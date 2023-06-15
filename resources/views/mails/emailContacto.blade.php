30636643856f1a6d9cacf3a77291d5beaad865c8
unknown <alexander.br159@gmail.com>
2023-06-15 18:31:57 +0200

    sec commit

--- resources/views/mails/emailContacto.blade.php
+++ resources/views/mails/emailContacto.blade.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>información de Contacto</title>
+    <title>Información de Contacto</title>
+    <!-- Incluir Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h3>{{$nombre}}</h3>
-    <p>{{$email}}</p>
-    <p>{{$descripcion}}</p>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <h1 class="card-title">Informacion de Contacto</h1>
+                <h3 class="card-title">Nombre: {{$nombre}}</h3>
+                <p class="card-text">Correo: {{$email}}</p>
+                <p class="card-text">Descripcion: {{$descripcion}}</p>
+            </div>
+        </div>
+    </div>
+    <!-- Incluir Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
